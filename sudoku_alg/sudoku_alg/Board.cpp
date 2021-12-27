e4403f892d6bf217151e27eb326e1b327f8810bf
maya_tavor <maya.tavor.04@gmail.com>
2021-12-27 22:54:44 +0200

    added the constructor and 3 more small functions

--- sudoku_alg/sudoku_alg/Board.cpp
+++ sudoku_alg/sudoku_alg/Board.cpp
@@ -2,20 +2,48 @@
 
 Board::Board(std::string b)
 {
+    int count = 0;
+    for (size_t i = 0; i < BOARD_SIZE; i++)
+    {
+        for (size_t j = 0; j < BOARD_SIZE; j++)
+        {
+            this->board[i][j] = b[count];
+            count++;
+        }
+    }
+
+    for (size_t i = 0; i < BOARD_SIZE; i++)
+    {
+        this->number_amount[i] = this->CountNumber(b, i+1);
+    }
 }
 
 bool Board::AddNumber(int number, int r, int c)
 {
-    return false;
+    if (this->board[r][c] == 0)
+    {
+        this->board[r][c] = number;
+        this->number_amount[number - 1] = this->number_amount[number - 1] + 1;
+    }
 }
 
 bool Board::CheckForRow(int rNum, int num)
 {
+    for (size_t i = 0; i < BOARD_SIZE; i++)
+    {
+        if (this->board[rNum][i] == num)
+            return true;
+    }
     return false;
 }
 
 bool Board::CheckForColumn(int cNum, int num)
 {
+    for (size_t i = 0; i < BOARD_SIZE; i++)
+    {
+        if (this->board[i][cNum] == num)
+            return true;
+    }
     return false;
 }
 
@@ -38,3 +66,11 @@ bool Board::AddMissingC(int cNum)
 {
     return false;
 }
+
+
+//helpers
+int Board::CountNumber(std::string b, int number)
+{
+    return std::count(b.begin(), b.end(), number);
+}
+
--- sudoku_alg/sudoku_alg/Board.h
+++ sudoku_alg/sudoku_alg/Board.h
@@ -1,20 +1,43 @@
 #pragma once
 
 #include <iostream>
+#include <algorithm>
 
+#define BOARD_SIZE 9
 
 class Board
 {
 public:
 	Board(std::string b);
 
+	/*
+	if the program found where to put a number, the functio will put the number in the correct place and add 1 to the number count
+	input: the number to add, the row number and the column number
+	output: if the number was added successfully or not
+	*/
 	bool AddNumber(int number, int r, int c);
+
+	/*
+	this functio checks if the number is in the row
+	input: the number and the row
+	output: if the number already exists in the row or not
+	*/
 	bool CheckForRow(int rNum, int num);
+
+	/*
+	this functio checks if the number is in the column
+	input: the number and the column
+	output: if the number already exists in the column or not
+	*/
 	bool CheckForColumn(int cNum, int num);
 	int HowManyMissingR(int rNum);
 	int HowManyMissingC(int cNum);
 	bool AddMissingR(int rNum);
 	bool AddMissingC(int cNum);
+
+
+	//starters
+	int CountNumber(std::string b, int number);
 
 private:
 	int board[9][9];
